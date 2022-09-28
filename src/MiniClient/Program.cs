1aecd0f09a33d6c6aca6ed2d1b684d2096372fec
Janne Mattila <janne.mattila@hotmail.fi>
2022-09-28 21:32:53 +0300

    Add mini app placeholders

---
+++ src/MiniClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using Mini;
+
+Console.WriteLine("Mini Client");
+
+var server = "https://localhost:7201";
+
+try
+{
+    var httpClient = new HttpClient();
+    var channel = GrpcChannel.ForAddress(server,
+        new GrpcChannelOptions
+        {
+            HttpClient = httpClient
+        });
+    var client = new Greeter.GreeterClient(channel);
+    var reply = await client.SayHelloAsync(
+        new HelloRequest
+        {
+            Name = "GreeterClient"
+        });
+
+    Console.WriteLine("Greeting: " + reply.Message);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
