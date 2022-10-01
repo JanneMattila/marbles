4fd478ce8b76905c928a9f9c0be5917f4a0f219c
Janne Mattila <janne.mattila@hotmail.fi>
2022-10-01 14:50:34 +0300

    Add self signed certificats

--- doc/misc.md
+++ doc/misc.md
@@ -11,3 +11,32 @@ Using https://favicon.io/favicon-generator/
 - 110
 - #FFFFFF
 - #209CEE
+
+## Certificates
+
+```bash
+# Generate Certificate Authority (CA)
+openssl req -x509 -sha256 -days 3650 -newkey -subj "/CN=demos" rsa:2048 -keyout ca.key -out ca.crt
+
+# Create server Certificate Signing Request (CSR) configuration file
+cat > server.conf <<EOF
+[ req ]
+default_bits = 2048
+prompt = no
+default_md = sha256
+distinguished_name = dn
+
+[ dn ]
+CN = server
+
+EOF
+
+# Generate server private key
+openssl genrsa -out server.key 2048
+
+# Generate Certificate Signing Request (CSR) using server private key and configuration file
+openssl req -new -key server.key -out server.csr -config server.conf
+
+# Generate certificate with self signed ca
+openssl x509 -req -in server.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 3650 -sha256
+```
--- src/MiniClient/Program.cs
+++ src/MiniClient/Program.cs
@@ -1,13 +1,43 @@
 ﻿using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
 using Mini;
+using System.Security.Cryptography.X509Certificates;
 
 Console.WriteLine("Mini Client");
 
-var server = "https://localhost:7201";
+var builder = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+#if DEBUG
+    .AddUserSecrets<Program>()
+#endif
+    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+var configuration = builder.Build();
+
+var server = configuration.GetValue<string>("server", "https://localhost:5001");
+var caCertFilePath = configuration.GetValue<string>("ca_cert_path");
+
+if (!File.Exists(caCertFilePath))
+{
+    Console.WriteLine($"CA certificate file not found: {caCertFilePath}");
+    return;
+}
 
 try
 {
-    var httpClient = new HttpClient();
+    var httpClient = new HttpClient(new HttpClientHandler
+    {
+        CheckCertificateRevocationList = false,
+        ServerCertificateCustomValidationCallback = (message, cert, chain, _) =>
+        {
+            ArgumentNullException.ThrowIfNull(chain);
+            ArgumentNullException.ThrowIfNull(cert);
+            chain.ChainPolicy.TrustMode = X509ChainTrustMode.CustomRootTrust;
+            chain.ChainPolicy.CustomTrustStore.Add(new X509Certificate2(caCertFilePath));
+            return chain.Build(cert);
+        }
+    });
     var channel = GrpcChannel.ForAddress(server,
         new GrpcChannelOptions
         {
