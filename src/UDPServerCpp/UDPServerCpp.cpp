f3bb137a6682eb29bf363100feee4e3426bdd96d
Janne Mattila <janne.mattila@hotmail.fi>
2024-07-17 20:24:12 +0300

    Add config file

--- src/UDPServerCpp/UDPServerCpp.cpp
+++ src/UDPServerCpp/UDPServerCpp.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#include <windows.h> // Include Windows.h for GetPrivateProfileString
+#include <cwchar>
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -23,10 +25,30 @@ int main()
         return 1;
     }
 
+    // Read port number from config.ini
+    wchar_t exePath[MAX_PATH];
+    GetModuleFileName(NULL, exePath, MAX_PATH);
+
+    // Extract directory from exePath
+    wchar_t* lastSlash = wcsrchr(exePath, L'\\');
+    if (lastSlash) {
+        *(lastSlash + 1) = L'\0'; // Terminate the string right after the last slash
+    }
+
+    // Append config.ini to the directory path
+    wcscat_s(exePath, MAX_PATH, L"config.ini");
+    std::wcout << exePath << std::endl;
+
+    // Now exePath contains the full path to config.ini
+    wchar_t portStr[6];
+    GetPrivateProfileString(L"Settings", L"Port", L"5556", portStr, 6, exePath);
+
+    int port = std::wcstol(portStr, nullptr, 10); // Convert port number string to integer
+
     sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET; // IPv4
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface
-    servaddr.sin_port = htons(5555); // Port number
+    servaddr.sin_port = htons(port); // Port number read from config.ini
 
     // Bind the socket with the server address
     if (bind(sockfd, (SOCKADDR*)&servaddr, sizeof(servaddr)) == SOCKET_ERROR) {
@@ -36,7 +58,7 @@ int main()
         return 1;
     }
 
-    std::cout << "UDP Server listening on port 5555" << std::endl;
+    std::cout << "UDP Server listening on port " << port << std::endl;
 
     while (true) {
         char buffer[1024];
